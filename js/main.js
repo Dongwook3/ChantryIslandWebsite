655fd06838487cba86cd2c1b589c923bf63c11a0
Dongwook3 <d_shin2@fanshaweonline.ca>
2017-03-16 00:41:53 -0400

    Add files via upload
    modified main.js

--- js/main.js
+++ js/main.js
@@ -23,9 +23,9 @@ $(document).foundation();
 
 	//map pop out
 	function popUp(url){
-		newwindow = window.open(url,'test','height=830,width=800');
+		newwindow = window.open(url,'test','height=500,width=800');
 		if (window.focus){
-			newwindow.focus();	
+			newwindow.focus()	
 		}
 		return false;
 	}
@@ -34,11 +34,11 @@ $(document).foundation();
 	// map
 (function(){
 	var map = new google.maps.Map(document.querySelector('.map')), 
-		//loading = document.querySelector('.loading'),
-		panorama = new google.maps.StreetViewPanorama(document.querySelector('.pano')),
+		loading = document.querySelector('.loading'),
 
 		geocoder = new google.maps.Geocoder(),
 		mapButton = document.querySelector('.drawRoute'),
+		mapsButton = document.querySelector('.drawRoute2'),
 
 		directionsService = new google.maps.DirectionsService(),
 		directionsDisplay,
@@ -55,21 +55,9 @@ $(document).foundation();
 		directionsDisplay = new google.maps.DirectionsRenderer();
 		directionsDisplay.setMap(map);
 
-		var sv = new google.maps.StreetViewService();
-
-		map.setCenter({ lat: 44.500216, lng: -81.373118 });//({ lat: position.coords.latitude, lng: position.coords.longitude });
+		map.setCenter({lat: 44.500216, lng: -81.373118});
 
 		map.setZoom(13);
-
-		// Set the initial Street View camera to the center of the map
-        sv.getPanorama({location: { lat: 44.500216, lng: -81.373118 }, radius: 50}, processSVData);
-
-        // Look for a nearby Street View panorama when the map is clicked.
-        // getPanoramaByLocation will return the nearest pano when the
-        // given radius is 50 meters or less.
-        map.addListener('click', function(event) {
-          sv.getPanorama({location: event.latLng, radius: 50}, processSVData);
-        });
 
 		yourMarker = new google.maps.Marker({
 			position : { lat: position.coords.latitude, lng: position.coords.longitude },
@@ -78,72 +66,46 @@ $(document).foundation();
 			title: 'Your Location'
 		});
 		
-		var chantryIsland = new google.maps.Marker({
-			position : {lat: 44.49008, lng: -81.40386 },
-			map:map,
-			title: 'Chantry Island Lighthouse',
-			icon: 'images/icons/chantry.png'
-		});
+	var chantryIsland = new google.maps.Marker({
+		position : {lat: 44.49008, lng: -81.40386 },
+		map:map,
+		title: 'Chantry Island Lighthouse',
+		icon: 'images/icons/chantry.png'
+	});
 
-	    var chantryInfo = '<div id="content">' + '<h2><b>Chantry Island Lighthouse</b></h2>' + '<p>Chantry Island is located on Lake Huron, just over a mile southwest of the Saugeen River mouth in Southampton, Ontario.</p>' + '</div>';
+			var chantryInfo = '<div id="content">' + '<h2><b>Chantry Island Lighthouse</b></h2>' + '<p>Chantry Island is located on Lake Huron, just over a mile southwest of the Saugeen River mouth in Southampton, Ontario.</p>' + '</div>';
 	
-		var chantryWindow = new google.maps.InfoWindow({
-			content: chantryInfo
-		});
+			var chantryWindow = new google.maps.InfoWindow({
+				content: chantryInfo
+			});
 			
-		chantryIsland.addListener('click', function() {
-			chantryWindow.open(map, chantryIsland);		
-		});
+			chantryIsland.addListener('click', function() {
+				chantryWindow.open(map, chantryIsland);		
+			});
 		
-		var tourBase = new google.maps.Marker({
-			position: {lat: 44.500216, lng: -81.373118},
-			map:map,
-			title: 'Chantry Island Tour Information',
-			icon: 'images/icons/tourBase.png'
-		});
 	
-		var tourInfo = '<div id="content">' + '<h2><b>Chantry Island Tour Base and Gift Shop</b></h2>' + '<p>Located at the south side of the Saugeen River at the harbour in Southampton. Gift shop and tour bookings available here. See <a href="#">www.ChantryIsland.com</a> for more information.</a></p>' + '</div>';
+	var tourBase = new google.maps.Marker({
+		position: {lat: 44.500216, lng: -81.373118},
+		map:map,
+		title: 'Chantry Island Tour Information',
+		icon: 'images/icons/tourBase.png'
+	});
+	
+			var tourInfo = '<div id="content">' + '<h2><b>Chantry Island Tour Base and Gift Shop</b></h2>' + '<p>Located at the south side of the Saugeen River at the harbour in Southampton. Gift shop and tour bookings available here. See <a href="#">www.ChantryIsland.com</a> for more information.</a></p>' + '</div>';
 			
-		var tourWindow = new google.maps.InfoWindow({
-			content: tourInfo
-		});
+			var tourWindow = new google.maps.InfoWindow({
+				content: tourInfo
+			});
 			
-		tourBase.addListener('click', function() {
-			tourWindow.open(map, tourBase);		
-		});
+			tourBase.addListener('click', function() {
+				tourWindow.open(map, tourBase);		
+			});
 
-	loading.classList.add('.hide-loading');
+
+	loading.classList.add('hide-loading');
 	}
-	
-	function processSVData(data, status) {
-        if (status === 'OK') {
-          var marker = new google.maps.Marker({
-            position: data.location.latLng,
-            map: map,
-            title: data.location.description
-          });
 
-          panorama.setPano(data.location.pano);
-          panorama.setPov({
-            heading: 270,
-            pitch: 0
-          });
-          panorama.setVisible(true);
 
-          marker.addListener('click', function() {
-            var markerPanoID = data.location.pano;
-            // Set the Pano to use the passed panoID.
-            panorama.setPano(markerPanoID);
-            panorama.setPov({
-              heading: 270,
-              pitch: 0
-            });
-            panorama.setVisible(true);
-          });
-        } else {
-          console.error('Street View data not found for this location.');
-        }
-      }
 
 	function drawRoute() {
 		var start = yourLocation;
@@ -156,9 +118,8 @@ $(document).foundation();
 
 		directionsService.route(request, function(response, status) {
 			if (status === 'OK') {
-				//document.getElementById('time').innerHTML = (response.routes[0].legs[0].distance.value / 1000 / 110).toFixed(2)+ " hours " + " or "+ (response.routes[0].legs[0].distance.value / 1000 / 110 * 60).toFixed(0)+ " minutes.";
 				var arrivalTime = (response.routes[0].legs[0].distance.value / 1000 / 110 * 60).toFixed(0);
-				document.getElementById('time').innerHTML = parseInt(arrivalTime / 60) + " h " + arrivalTime % 60 + " m";
+				document.querySelector('#time').innerHTML = parseInt(arrivalTime / 60) + " h " + arrivalTime % 60 + " m";
 				directionsDisplay.setDirections(response);
 			}
 		});
@@ -176,5 +137,5 @@ $(document).foundation();
 	}
 
 	mapButton.addEventListener('click', drawRoute, false);
-})();
 
+})();
